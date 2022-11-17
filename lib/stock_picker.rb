b7332258e4d00027f5ab231568900b756563029a
BrendanFreeman1 <vollak1@gmail.com>
2022-11-18 10:37:57 +1100

    Completed stock_picker exercise

--- lib/stock_picker.rb
+++ lib/stock_picker.rb
@@ -1,5 +1,25 @@
 def stock_picker(prices)
+  buy_pointer = 0
+  sell_pointer = 1
+  sell_difference = 0
+  buy_day = nil
+  sell_day = nil
 
+  while(buy_pointer < prices.length - 1) do
+    while(sell_pointer < prices.length) do
+      if((prices[sell_pointer] - prices[buy_pointer]) > sell_difference)
+
+        sell_difference = prices[sell_pointer] - prices[buy_pointer]
+        buy_day = buy_pointer
+        sell_day = sell_pointer
+
+      end
+      sell_pointer += 1
+    end
+    buy_pointer += 1
+    sell_pointer = buy_pointer + 1
+  end
+  return [buy_day, sell_day]
 end
 
-puts stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([17,3,6,9,15,8,6,1,19])
