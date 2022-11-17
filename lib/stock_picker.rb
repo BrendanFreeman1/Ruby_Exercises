c76a8c49cf9f2c08700ed1fe6c15b1e88c5ede18
BrendanFreeman1 <vollak1@gmail.com>
2022-11-18 10:41:00 +1100

    Added assignment text

--- lib/stock_picker.rb
+++ lib/stock_picker.rb
@@ -1,3 +1,8 @@
+#Implement a method #stock_picker that takes in an array of stock prices, one for each hypothetical day. 
+#It should return a pair of days representing the best day to buy and the best day to sell. 
+#Days start at 0. You need to buy before you can sell
+
+
 def stock_picker(prices)
   buy_pointer = 0
   sell_pointer = 1
