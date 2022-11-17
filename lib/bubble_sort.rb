9b555b2d88916b8f3742153b82bac6339715866b
BrendanFreeman1 <vollak1@gmail.com>
2022-11-18 10:57:52 +1100

    Create bubble_sort exercise file

---
+++ lib/bubble_sort.rb
@@ -0,0 +1,9 @@
+#Build a method #bubble_sort that takes an array and returns a sorted array
+#using the bubble sort method where each element is compared to the one next 
+#to it and they are swapped if the one on the left is larger than the one on the right.
+
+def bubble_sort(array)
+  
+end
+
+puts bubble_sort([0,2,2,3,4,78])
