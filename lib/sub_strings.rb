31e7aa098bd2a4297cbde576658beb95284e5567
BrendanFreeman1 <vollak1@gmail.com>
2022-11-17 16:36:49 +1100

    Completed sub_strings exercise

--- lib/sub_strings.rb
+++ lib/sub_strings.rb
@@ -2,20 +2,15 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 def sub_strings(string, dictionary)
   result = Hash.new(0)
-  input_words = string.downcase.split(" ")
+  string = string.downcase
 
-  input_words.each do |w| 
-    w = w.gsub(/[^A-Za-z]/,'')
-
-    dictionary.each do |word| 
-      if(word == w)
-        result[word] += 1
-      end
+  dictionary.each do |word| 
+    if(string.include?(word))
+      result[word] += 1
     end
   end
 
 return result.sort
-
 end
 
 puts sub_strings("Howdy partner, sit down! How's it going?", dictionary)
