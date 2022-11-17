de3aa0d752c33c83032d8328145b8466a3483476
BrendanFreeman1 <vollak1@gmail.com>
2022-11-17 15:15:51 +1100

    first commit

---
+++ caesar_cipher.rb
@@ -0,0 +1,37 @@
+def caesar_cipher(text, shift)
+  shifted_text = ""
+  shift = adjust_shift(shift)
+
+  text.each_char do |c| 
+    if(c.ord.between?(65, 90))
+      c = contain_number(65, 90, c.ord + shift).chr
+    end
+    if(c.ord.between?(97, 122))
+      c = contain_number(97, 122, c.ord + shift).chr
+    end
+
+    shifted_text += c
+  end
+
+  return shifted_text
+end
+
+def contain_number(lower, upper, current)
+  if(current > upper)
+    current -= 1
+    current = lower + (current - upper)
+  end
+  if(current < lower)
+    current += 1
+    current = upper - (lower - current)
+  end
+
+  return current
+end
+
+def adjust_shift(shift)
+  if(shift < 0) then return (shift.abs % 26) * -1 else return shift % 26 end
+end
+
+
+puts caesar_cipher("hello world", -87)
