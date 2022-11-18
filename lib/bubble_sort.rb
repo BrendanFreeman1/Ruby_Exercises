474df519edee4a9d1dc213533552bb3d85662f56
BrendanFreeman1 <vollak1@gmail.com>
2022-11-18 11:46:43 +1100

    Completed bubble_sort

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -3,7 +3,22 @@
 #to it and they are swapped if the one on the left is larger than the one on the right.
 
 def bubble_sort(array)
-  
+  array_end = array.length - 2
+  n = 0
+
+  #n reduces how far the sort moves through the array by 1 each iteration.
+  #After each pass the highest number found will be at the end of the array
+  #and wont need to be checked again.
+  while (n < array.length) do
+    for i in 0..array_end - n
+      #If the left element is greater than the right element, swap them
+      if(array[i] > array[i+1])
+        array[i], array[i+1] = array[i+1], array[i]
+      end
+    end
+    n += 1
+  end
+  return array
 end
 
-puts bubble_sort([0,2,2,3,4,78])
+p bubble_sort([4,3,78,2,0,2])
